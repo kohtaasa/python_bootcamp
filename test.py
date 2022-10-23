d81d28fddf9b86394e71ade4828090d4283dface
zoohta <koh.0430.ta@gmail.com>
2022-10-23 22:41:20 +0900

    modified

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
-import doctest
+from doctest import product
 import unittest2
+
 
 class Test(unittest2.TestCase):
     def test_product(self):
-        self.assartEqual(self.product(2, 5), 10)
+        self.assartEqual(product(2, 5), 10)
+
+
+if __name__ == '__main__':
+    unittest2.main()
+
 
 
